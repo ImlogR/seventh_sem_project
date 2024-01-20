c5c7d70c1a92b4a95d8d12e3d85f97a7a79dbda6
robeen <robeen.smiles@gmail.com>
2024-01-20 21:32:05 +0545

    playsound

--- main.py
+++ main.py
@@ -2,10 +2,46 @@ import cv2
 import imutils
 from flask import Flask, render_template, Response
 import torch
-import pandas as pd 
+# import pandas as pd 
+import threading   # Library for threading -- which allows code to run in backend
+import playsound   # Library for alarm sound
+import smtplib     # Library for email sending
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Initializing the Flask app
 app = Flask(__name__)
+
+fire_cascade = cv2.CascadeClassifier('fire_detection.xml') # To access xml file which includes positive and negative images of fire. (Trained images)
+
+# vid = cv2.VideoCapture(0) # To start camera this command is used "0" for laptop inbuilt camera and "1" for USB attahed camera
+
+def play_alarm_sound_function(): # defined function to play alarm post fire detection using threading
+    playsound.playsound('fire_Alarm.mp3',True) # to play alarm 
+    print("Fire alarm end") # to print in console
+
+def send_mail_function(): # defined function to send mail post fire detection using threading
+    
+    # recipientmail = recipientmail.lower() # To lower case mail
+    recipientmail = "nikitarai0000@gmail.com" # recipients mail
+    
+    try:
+        sender_mail = os.getenv("SENDER_EMAIL")
+        sender_password = os.getenv("SENDER_PASSWORD")
+        if not sender_mail or not sender_password:
+            print("Error: Sender email or password not provided in environment variables.")
+            return
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(sender_mail, sender_password) # Senders mail ID and password
+        server.sendmail('add recipients mail', recipientmail, "Warning fire accident has been reported") # recipients mail with mail message
+        print("Alert mail sent sucesfully to {}".format(recipientmail)) # to print in consol to whome mail is sent
+        server.close() ## To close server
+        
+    except Exception as e:
+        print(e) # To print error if any
 
 # Load the YOLOv5 model into the system
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")
@@ -15,7 +51,36 @@ camera = cv2.VideoCapture(0)
 
 # Function to generate video frames through webcam
 def generate_frames():
+    # initial code
+    runOnce = False # created boolean
     while True:
+        Alarm_Status = False
+        ret, frame2 = camera.read() # Value in ret is True # To read video frame
+        gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY) # To convert frame into gray color
+        fire = fire_cascade.detectMultiScale(frame2, 1.2, 5) # to provide frame resolution
+
+        ## to highlight fire with square 
+        for (x,y,w,h) in fire:
+            cv2.rectangle(frame2,(x-20,y-20),(x+w+20,y+h+20),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame2[y:y+h, x:x+w]
+            print(1)
+
+            print("Fire alarm initiated")
+            threading.Thread(target=play_alarm_sound_function).start()  # To call alarm thread
+
+            if runOnce == False:
+                print("Mail send initiated")
+                threading.Thread(target=send_mail_function).start() # To call alarm thread
+                runOnce = True
+            if runOnce == True:
+                print("Mail is already sent once")
+                runOnce = True
+
+        cv2.imshow('frame', frame2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        #initial code
         success, frame = camera.read()
         if not success:
             break
